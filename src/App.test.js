345344c8b68618edd8f1185367e157b690ff5feb
Fabian schmelzing <fabian.schmelzing@docc.techstarter.de>
2024-04-10 13:59:29 +0200

    chromedriver install

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,14 @@
-require("chromedriver");
-var assert = require('assert');
+const {Builder, By, Key, until} = require('selenium-webdriver');
 
-const {Builder, By, Key} = require('selenium-webdriver');
+async function example() {
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get('http://www.example.com');
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  } finally {
+    await driver.quit();
+  }
+}
 
-describe('Einkaufliste', function() {
-  it('Name hinzufügen', async function(){
-    let driver = new Builder().forBrowser('chrome').build();
-    await driver.get('http://shopwise.webhop.me/');
-    await driver.findElement(By.className("col-2 fs-5 rounded text-center border-0 w-25 p-2 einkäufer")).sendKeys("banana")
-    await driver.findElement(By.className("col-2 fs-5 rounded text-center border-0 w-26")).sendKeys("chiquita")
-    //
-    //await driver.findElement(By.id("menge")).selectByIndex(2)
-    await driver.findElement(By.className("btn btn-outline-warning add-button")).click()
-    await driver.findElement(By.className("bi bi-trash text-danger fs-4")).click()
-    //await driver.quit()
-  })
-})
+example();
